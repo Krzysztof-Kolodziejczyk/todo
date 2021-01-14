ebf40145dfa8bcd3f6b219c2e57b87e9943ff4fd
Krzysztof Kołodziejczyk <56395653+Krzysztof-Kolodziejczyk@users.noreply.github.com>
2021-01-14 17:12:11 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from datetime import datetime
+import tkinter as tk
 
-tk = Tk()
+root = Tk()
 
-tk.title("todo Project")
-tk.geometry("300x400")
-tk.configure(background="#94d3f7")
+root.title("todo Project")
+root.geometry("500x500")
+root.configure(background="#94d3f7")
 
 font = ("Courier", 15, "italic")
 
 # creating app frame
 
-box_frame = Frame()
+box_frame = tk.Canvas(root, width=700, height=700)
 box_frame.pack()
 
 # creating listbox
@@ -19,7 +20,7 @@ box_frame.pack()
 list_box = Listbox(
     box_frame,
     font=font,
-    width=28,
+    width=50,
     height=9,
     bg="SystemButtonFace",
     bd=3,
@@ -28,7 +29,7 @@ list_box = Listbox(
     background="white"
 )
 
-list_box.pack(side=RIGHT, fill=BOTH)
+list_box.pack(side=LEFT, fill=BOTH)
 
 tasks_list = []
 
@@ -46,14 +47,14 @@ scrollBar.config(command=list_box.yview())
 
 # create entry box to add item to the list
 entry = Entry(
-    tk,
+    root,
     font=("Helveticka", 24)
 )
 
 entry.pack(pady=10)
 
 # Create Button Frame
-button_Frame = Frame(tk)
+button_Frame = Frame(root)
 button_Frame.pack(pady=3)
 
 
@@ -77,11 +78,12 @@ def delete_item():
 
 
 def add_item():
-    current_index = list_box.index(END)
-    current_index += 1
-    current_text = str(current_index) + ". " + entry.get() + "       " + datetime.now().strftime("%m/%d/%Y")
-    list_box.insert(END, current_text)
-    entry.delete(0, END)
+    if entry.get() != "":
+        current_index = list_box.index(END)
+        current_index += 1
+        current_text = str(current_index) + ". " + entry.get() + "       " + datetime.now().strftime("%m/%d/%Y")
+        list_box.insert(END, current_text)
+        entry.delete(0, END)
 
 
 def mark_item():
@@ -89,7 +91,7 @@ def mark_item():
         list_box.curselection(),
         fg="green"
     )
-    list_box.selection_clear(0,END)
+    list_box.selection_clear(0, END)
 
 
 def unmarked_item():
@@ -97,7 +99,7 @@ def unmarked_item():
         list_box.curselection(),
         fg="black"
     )
-    list_box.selection_clear(0,END)
+    list_box.selection_clear(0, END)
 
 
 # function to move marked element in front of a list
@@ -114,13 +116,22 @@ def priority_item():
     list_box.insert(0, tmp)
 
 
+def deadline_item():
+    element = list_box.get(ANCHOR)
+    deadline_data = deadline_entry.get()
+    new_element = element + "   " + deadline_data
+    index = list_box.index(ANCHOR)
+    list_box.delete(index)
+    list_box.insert(index, new_element)
+
+
 # buttons
 delete_button = Button(button_Frame, text="usuń", command=delete_item)
 add_button = Button(button_Frame, text="dodaj ", command=add_item)
 mark_button = Button(button_Frame, text="ukończone", command=mark_item)
 unmarked_button = Button(button_Frame, text="odznacz", command=unmarked_item)
 priority_button = Button(button_Frame, text="nadaj priorytet", command=priority_item)
-
+deadline_button = Button(button_Frame, text="dodaj deadline", command=deadline_item)
 
 # placing buttons on a gird
 add_button.grid(row=0, column=0, pady=3)
@@ -128,5 +139,11 @@ delete_button.grid(row=1, column=0)
 mark_button.grid(row=2, column=0, pady=3)
 unmarked_button.grid(row=3, column=0)
 priority_button.grid(row=4, column=0, pady=3)
+deadline_button.grid(row=5, column=0, pady=10)
+
+deadline_entry = tk.Entry(root)
+box_frame.create_window(300, 500, window=deadline_entry)
+
+deadline_entry.pack()
 
 tk.mainloop()
